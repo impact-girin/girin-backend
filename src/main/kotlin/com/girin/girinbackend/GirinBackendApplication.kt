85c3ec87acd58d2ede0a46d8cccc0e9d950cac07
이정호 <dlwjdgh1115@gmail.com>
2024-01-27 13:09:19 +0900

    project setting

---
+++ src/main/kotlin/com/girin/girinbackend/GirinBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.girin.girinbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GirinBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GirinBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/girin/girinbackend/GirinBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.girin.girinbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GirinBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
