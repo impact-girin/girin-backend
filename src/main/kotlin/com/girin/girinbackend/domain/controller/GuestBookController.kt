6e4ebc5563137dab169911e0ce9270bc8fa52a00
이정호 <dlwjdgh1115@gmail.com>
2024-01-28 08:26:42 +0900

    feat : add guest book

--- src/main/kotlin/com/girin/girinbackend/domain/controller/GuestBookController.kt
+++ src/main/kotlin/com/girin/girinbackend/domain/controller/GuestBookController.kt
@@ -3,6 +3,8 @@ package com.girin.girinbackend.domain.controller
 import com.girin.girinbackend.domain.controller.dto.response.GuestBookList
 import com.girin.girinbackend.domain.service.GuestBookService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +17,12 @@ class GuestBookController(
     @GetMapping("/list")
     fun getGuestBookList(): GuestBookList {
         return guestBookService.getGuestBookList()
+    }
+
+    @PostMapping("/add/{mountain-id}")
+    fun createGuestBook(
+        @PathVariable("mountain-id") mountainId: Long,
+    ) {
+        guestBookService.createGuestBook(mountainId)
     }
 }
--- src/main/kotlin/com/girin/girinbackend/domain/service/GuestBookService.kt
+++ src/main/kotlin/com/girin/girinbackend/domain/service/GuestBookService.kt
@@ -1,12 +1,19 @@
 package com.girin.girinbackend.domain.service
 
+import com.girin.girinbackend.common.exception.MountainNotFoundException
 import com.girin.girinbackend.domain.controller.dto.response.GuestBookList
+import com.girin.girinbackend.domain.entity.guestbook.GuestBook
+import com.girin.girinbackend.domain.facade.UserFacade
 import com.girin.girinbackend.domain.repository.GuestBookRepository
+import com.girin.girinbackend.domain.repository.MountainRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class GuestBookService(
     private val guestBookRepository: GuestBookRepository,
+    private val mountainRepository: MountainRepository,
+    private val userFacade: UserFacade,
 ) {
 
     fun getGuestBookList(): GuestBookList {
@@ -21,5 +28,19 @@ class GuestBookService(
         }
 
         return GuestBookList(response)
+    }
+
+    fun createGuestBook(mountainId: Long) {
+        val user = userFacade.getCurrentUser()
+
+        val mountain = mountainRepository.findByIdOrNull(mountainId)
+            ?: throw MountainNotFoundException
+
+        guestBookRepository.save(
+            GuestBook(
+                user = user,
+                mountain = mountain,
+            )
+        )
     }
 }
